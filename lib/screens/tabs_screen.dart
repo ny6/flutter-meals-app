405738c5ab232a300c9912ca2ea4127b031f5f39
Yeshu Kochher <yeshu@outlook.in>
2020-01-10 16:22:05 +0530

    one where bottom tab screen added

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,26 +9,40 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'title': 'Category', 'page': CategoryScreen()},
+    {'title': 'Favorite', 'page': FavoriteScreen()},
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.category), text: 'Categories'),
-              Tab(icon: Icon(Icons.star), text: 'Favorites'),
-            ],
+    return Scaffold(
+      appBar: AppBar(title: Text(_pages[_selectedPageIndex]['title'])),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.category),
+            title: const Text('Categories'),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoryScreen(),
-            FavoriteScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.star),
+            title: const Text('Favorites'),
+          ),
+        ],
       ),
     );
   }
